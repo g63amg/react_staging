606d5f744ea22df2ff83dca17af65f2a531ccbb4
Fuhua <290855872@qq.com>
2023-02-03 00:29:43 +0800

    react 笔记

--- 20_src_withRouter的使用/App.js
+++ 20_src_withRouter的使用/App.js
@@ -47,7 +47,7 @@ render() {
     }
 /*
 Redirect的使用
-1.一般卸载所有路由注册的最下方，当所有路由都无法匹配时，跳转到Redirect指定的路由
+1.一般写在所有路由注册的最下方，当所有路由都无法匹配时，跳转到Redirect指定的路由
 <Switch>
     <Route path="/about" component={About}/>
     <Route path="/home" component={Home}/>
