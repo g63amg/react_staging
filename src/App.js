237d22fb70e80c0205516aa321a8eb49dc013c53
Fuhua <hua.fu@cheilpengtai.com>
2023-02-03 20:55:02 +0800

    react 笔记

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React,{Component} from "react";
-import Count from "./components/Count/Count";
+import Count from "./containers/Count/Containers_Count";
+//引入redux中store
+import store from "./redux/store"
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Count/>
+        <Count store={store}/>
       </div>
     )
   }
